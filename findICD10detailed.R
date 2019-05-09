c32e400230892f29cc178b878c25f15daf96fda7
soffiafdz <so1.618e@gmail.com>
2019-05-09 16:16:50 +0000

    Detailed find function

---
+++ findICD10detailed.R
@@ -0,0 +1,9 @@
+## Detailed findICD10 function 
+
+findICD10 <- function(ICD9code, children = F) {
+  GEM <- readRDS('GEM.RDS') #Load GEM data
+  ## Rows that have that ICD9code
+  rows <- which(GEM[, 1] == as.character(ICD9code)) 
+  ## The ICD10 and flags for those rows
+  return(GEM[rows,c(2, 4:8)])
+}
