f0eb3852aea72fc58d67ef25da48b834457ce954
soffiafdz <so1.618e@gmail.com>
2019-05-09 16:42:02 -0400

    Working draft

--- flagDecisionTree.R
+++ flagDecisionTree.R
@@ -2,19 +2,25 @@
 
 
 DecisionTree <- function(ICD9code){
-  x <- findICD10(ICD9code)
-}
-if (!x$approximate){ #IF approximate is 0
-  ## Exact output 1 = 1 
-  out <- data.frame(ICD10 = x$ICD10, Details = "Exact Match") 
-} else { 
-  if (x$noMap){ #IF noMap is 1
-    ## Not available converstion 
-    out <- data.frame(ICD10 = NA, Details = "Not available conversion")
-  } else if (!x$combination){ #IF combination is 0 
-    ## Output will be only one, but is NOT EXACT 
-    out <- data.frame(ICD10 = x$ICD10, Details = "Approximate match")
-  } else {
-    ## Scenario and Choicelist mess
+  ## Can change ICD9code to the output of findICD10 -> Rename to X 
+  x <- findICD10(ICD9code)  # Remove if integrating to a bigger function
+  ## Create a base out... Probably remove for iterating. 
+  out <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), 
+                  c("Details", "ICD10"))
+  ### Decision tree 
+  if (!unique(x[,2])) {         # If exact match
+    out <- rbind(out, data.frame(Details = "Exact_match", ICD10 = x[,1]))
+  } else if (unique(x[,3])) {   # If no match
+    out <- rbind(out, data.frame(Details = "No_match", ICD10 = NA))
+  } else if (unique(!x[,4])) {  # If only one approximate
+    out <- rbind(out, 
+                 data.frame(Details = rep("Approximate_match", 
+                                          length(x[,1])),
+                            ICD10 = x$ICD10))
+  } else {                      # Then is a combination of codes
+    out <- rbind(out, data.frame(Details = rep("Combination", length(x[,1])),
+                          ICD10 = x[,1]))
+    out <- cbind2(out, x[,5:6])
   }
+  return(out)
 }
