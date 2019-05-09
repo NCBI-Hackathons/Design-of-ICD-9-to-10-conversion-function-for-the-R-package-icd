216389ec6510957a8c74687ea5fe82eb3497e372
rumarova <38050024+rumarova@users.noreply.github.com>
2019-05-09 10:45:51 -0400

    Add files via upload

---
+++ convertICD9toICD10.R
@@ -0,0 +1,58 @@
+#######################################################################################
+####          Data Loading
+######################################################################################
+
+codes <- read.table("./2018_I9gem.txt", header = T, 
+                    colClasses = "character")
+colnames(codes) <- c("ICD9", "ICD10", "flag")
+
+### this function returns posiible icd10
+convertICD9toICD10 <- function(icd, D) {
+  icd.version <- guess_version(icd)
+  
+  # check whether it's icd9 or icd10
+  if (icd.version == "icd9") {              # check if it's "icd9"
+    #print("OK!")
+    #return(icd)
+    
+    # check whether it's short or decimal
+    icd.format <- icd:::guess_short(icd)
+    if (icd.format == TRUE) {               # checks for short
+      icd.converted <- findICD10B(icd, D)      # if true, converts icd9 to icd10
+      return(icd.converted)                 # return the converted icd9 to icd10
+    } # if short or decimal 
+    else {                                  # for decimal format
+      icd.coverted <- decimal_to_short(icd) # converts decimal to short
+      icd.converted <- findICD10B(icd.converted, D)  # converts icd9 to icd10
+      return(icd.converted)                 # return the converted icd9 to icd10
+    } # else short or decimal
+  } # if icd9 or icd10
+  else {                                    # "icd10"
+    icd.format <- icd:::guess_short(icd)    # checks whether it's short or decimal
+    if (icd.format == TRUE) {               # if TRUE, returns icd10 in short format
+      return(icd)
+    } # if short or decimal
+    else {                                  # if FALSE
+      icd.short <- decimal_to_short(icd)    # converts decimal to short
+      return(icd.short)                     # returns icd10 in short format
+    } # else short or decimal
+  } # else icd 9 or icd10
+  
+} # end of function
+
+## Coding in base R.
+
+## function that inputs ICD9code and
+## D (conversion data) and outputs
+## the ICD9code also do you want children
+
+findICD10B <- function(ICD9code, D, children) {
+  
+  ## Rows that have that ICD9code
+  rows <- which(D[, "ICD9"] == ICD9code)
+  
+  ## The ICD10 and flags for those rows
+  ICD10code <- D[rows,c("ICD10", "flag")]
+  
+  return(ICD10code)
+}
