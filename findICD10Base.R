dcccf00dd1719a3a52781e4afc268e130b40f287
soffiafdz <so1.618e@gmail.com>
2019-05-09 15:12:38 +0000

    Changed D to GEM and added default value to children

--- findICD10Base.R
+++ findICD10Base.R
@@ -4,13 +4,13 @@
 ## D (conversion data) and outputs
 ## the ICD9code also do you want children
 
-findICD10B <- function(ICD9code, D, children) {
+findICD10B <- function(ICD9code, GEM, children = F) {
   
   ## Rows that have that ICD9code
-  rows <- which(D[, "ICD9"] == ICD9code)
+  rows <- which(GEM[, "ICD9"] == ICD9code)
   
   ## The ICD10 and flags for those rows
-  ICD10code <- D[rows,c("ICD10", "flag")]
+  ICD10code <- GEM[rows,c("ICD10", "flag")]
   
   return(ICD10code)
 }
