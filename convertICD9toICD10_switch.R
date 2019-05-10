cb3f6ef0905df92df3887379fb884d5834374607
nzgwynn <nzgwynn@gmail.com>
2019-05-10 11:06:06 -0400

    Update to base function

--- convertICD9toICD10_switch.R
+++ convertICD9toICD10_switch.R
@@ -5,8 +5,8 @@ convertICD9toICD10 <- function(icd) {
   icd.format <- icd:::guess_short(icd)
   icd.new <- paste0(icd.version,icd.format,sep = "")
   switch(icd.new,
-         "icd9TRUE" = return(findICD10(icd)),
-         "icd9FALSE" = return (findICD10(decimal_to_short(icd))),
+         "icd9TRUE" = return(findICD10B(icd)),
+         "icd9FALSE" = return (findICD10B(decimal_to_short(icd))),
          "icd10TRUE" = return(icd),
          "icd10FALSE" = return (decimal_to_short(icd))
          )
