404840bb3fa0c55ba92153b270f565c02ea93cb3
nzgwynn <nzgwynn@gmail.com>
2019-05-10 11:45:56 -0400

    Adding a line for readability

--- loadData.R
+++ loadData.R
@@ -6,6 +6,7 @@ GEM <- read.table("2018_I9gem.txt",
 
 # Extracting flags 
 colNames <- c('approximate','noMap','combination','scenario','choicelist')
+
 for (i in 1:5){
   GEM[,i+3] <- as.integer(substring(GEM[,3],i,i))
   names(GEM)[i+3] <- colNames[i]
