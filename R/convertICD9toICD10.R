64bb603809c4a44b3185477f9cf4fc7cffa40310
rumarova <38050024+rumarova@users.noreply.github.com>
2019-05-10 13:41:47 -0400

    Update convertICD9toICD10.R

--- R/convertICD9toICD10.R
+++ R/convertICD9toICD10.R
@@ -11,17 +11,27 @@ convertICD9toICD10 <- function(icd.table, flag=FALSE) {             # function
   } # if statement
   
   else {
-    output <- cbind(output, setNames(data.frame(matrix(ncol=3,nrow=0)),c('Details','Scenario','Choice_list')))
-    for (i in icd.table){
-      icd.converted = convert(i)
-      output <- rbind(output, data.frame(ICD_input=i,ICD10=icd.converted[1],Details=NA,Scenario=NA,Choice_list=NA))
-      flag.version <- guess_version(i)
-      if (flag.version == "icd9") {
-        flags <<- DecisionTree(i)
-      } # if
-      output <- merge(output, flags, sort=FALSE)
-    } # for loop
-    return(output)
+    output <- cbind(output, setNames(data.frame(matrix(ncol=3,nrow=0)),
+                                     c('Details','Scenario','Choice_list')))
+    #print(output)
+    
+     for (i in icd.table){
+       icd.converted = convert(i)
+       #print(icd.converted)
+       output <- rbind(output, data.frame(ICD_input=i,ICD10=icd.converted[1],Details=NA,
+                                          Scenario=NA,Choice_list=NA))
+       print(output)
+       flag.version <- guess_version(i)
+       if (flag.version == "icd9") {
+         flags <<- DecisionTree(i)
+         #print(flags)
+       } # if
+       
+       #output <- merge(output, flags, sort=FALSE)
+       print(output)
+     } # for loop
+     #return(output)
+     
   } # else
 } # convertICD9to ICD10 function 
 
@@ -30,6 +40,6 @@ convert <- function(icd) {
   icd.version <- guess_version(icd)
   switch(icd.version,
          "icd9" = return(findICD10(icd)),
-         "icd10" = return(c(icd, icd))
+         "icd10" = return(c(icd))
   )
 } # convert function
