877c6f9744b565c97f2b9f9160995c2ed1724c5f
soffiafdz <so1.618e@gmail.com>
2019-05-09 20:16:47 -0400

    Names out of loop

--- loadData.R
+++ loadData.R
@@ -5,8 +5,8 @@ GEM <- read.table("2018_I9gem.txt",
            col.names = c("ICD9", "ICD10", "flag"))
 
 # Extracting flags 
+colNames <- c('approximate','noMap','combination','scenario','choicelist')
 for (i in 1:5){
-  colNames <- c('approximate','noMap','combination','scenario','choicelist')
   GEM[,i+3] <- as.integer(substring(GEM[,3],i,i))
   names(GEM)[i+3] <- colNames[i]
 }
