84ea68bb66d732313d894844da24c5231b1df38e
soffiafdz <so1.618e@gmail.com>
2019-05-10 12:23:09 -0400

    missing parenthesis

--- R/convertICD9toICD10.R
+++ R/convertICD9toICD10.R
@@ -22,7 +22,7 @@ convertICD9toICD10 <- function(icd.table, flag=FALSE) {             # function
     for (i in icd.table){
       icd.converted = convert(i)
       flagsCols <- 
-      output <- cbind(output, setNames(data.frame(matrix(ncol=3,nrow=0)),c('Details','Scenario','Choice_list'))
+      output <- cbind(output, setNames(data.frame(matrix(ncol=3,nrow=0)),c('Details','Scenario','Choice_list')))
       output <- rbind(output, data.frame(ICD_input=i,ICD10=icd.converted[,1],Details=NA,Scenario=NA,Choice_list=NA))
       flag.version <- guess_version(i)
       if (flag.version == "icd9") {
